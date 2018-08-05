8cf94f5e9165e2f7e1816179d0891c5824fa7148
Krieger <krieger.j.cess@gmail.com>
2018-08-05 12:42:59 +0200

    change location to liege + max zoom

--- js/app.js
+++ js/app.js
@@ -1,350 +1,346 @@
-var map;
+var map
 
+var markers = []
 
-      var markers = [];
+//var polygon = null;
 
+var currentMarkers = []
+var placeMarkers = []
 
-      //var polygon = null;
-
-      var currentMarkers= [];
-      var placeMarkers = [];
-
-      function initMap() {
-
-        var styles = [
-          {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#e0efef"
-            }
-        ]
+function initMap() {
+  var styles = [
+    {
+      featureType: "landscape.natural",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          visibility: "on"
+        },
+        {
+          color: "#e0efef"
+        }
+      ]
     },
     {
-        "featureType": "poi",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#1900ff"
-            },
-            {
-                "color": "#c0e8e8"
-            }
-        ]
+      featureType: "poi",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          visibility: "on"
+        },
+        {
+          hue: "#1900ff"
+        },
+        {
+          color: "#c0e8e8"
+        }
+      ]
     },
     {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        {
+          lightness: 100
+        },
+        {
+          visibility: "simplified"
+        }
+      ]
     },
     {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off"
+        }
+      ]
     },
     {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 700
-            }
-        ]
+      featureType: "transit.line",
+      elementType: "geometry",
+      stylers: [
+        {
+          visibility: "on"
+        },
+        {
+          lightness: 700
+        }
+      ]
     },
     {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#7dcdcd"
-            }
-        ]
+      featureType: "water",
+      elementType: "all",
+      stylers: [
+        {
+          color: "#7dcdcd"
+        }
+      ]
+    }
+  ]
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 50.632885, lng: 5.579185 },
+    maxZoom: 17,
+    zoom: 14,
+    styles: styles,
+    mapTypeControl: false
+  })
+
+  var zoomAutocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("zoom-to-area-text")
+  )
+
+  zoomAutocomplete.bindTo("bounds", map)
+
+  var locations = [
+    { title: "Théâtre de Liège", location: { lat: 50.6405905, lng: 5.5747113 } },
+    { title: "Get your mug", location: { lat: 50.639573, lng: 5.573682 }},
+    { title: "Cathédrale Saint-Paul de Liège", location: { lat: 50.639573, lng: 5.573682 }},
+    { title: "Le Palais des Princes-Évêques", location: { lat: 50.6453812, lng: 5.5717773 } },
+    { title: "Le Pot au Lait", location: { lat: 50.6412571, lng: 5.5710652 } },
+    { title: "Cinéma Churchill", location: { lat: 50.6411689, lng: 5.5692158 } }
+  ]
+
+  var largeInfowindow = new google.maps.InfoWindow()
+
+  var defaultIcon = makeMarkerIcon("0091ff")
+
+  var highlightedIcon = makeMarkerIcon("FFFF24")
+
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location
+    var title = locations[i].title
+
+    var marker = new google.maps.Marker({
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
+      id: i
+    })
+
+    markers.push(marker)
+
+    marker.addListener("click", function() {
+      populateInfoWindow(this, largeInfowindow)
+    })
+
+    marker.addListener("mouseover", function() {
+      this.setIcon(highlightedIcon)
+    })
+    marker.addListener("mouseout", function() {
+      this.setIcon(defaultIcon)
+    })
+  }
+
+  document
+    .getElementById("show-listings")
+    .addEventListener("click", showListings)
+
+  document
+    .getElementById("hide-listings")
+    .addEventListener("click", function() {
+      hideMarkers(markers, currentMarkers)
+    })
+
+  document.getElementById("zoom-to-area").addEventListener("click", function() {
+    zoomToArea()
+  })
+
+  //TEST
+
+  //var one = 1;
+  document.getElementById("l1").addEventListener("click", showOne)
+  document.getElementById("l2").addEventListener("click", showOne)
+  document.getElementById("l3").addEventListener("click", showOne)
+  document.getElementById("l4").addEventListener("click", showOne)
+  document.getElementById("l5").addEventListener("click", showOne)
+  document.getElementById("l6").addEventListener("click", showOne)
+
+  function showOne() {
+    if (event.target.id === "l1") {
+      var index = 0
+    } else if (event.target.id === "l2") {
+      var index = 1
+    } else if (event.target.id === "l3") {
+      var index = 2
+    } else if (event.target.id === "l4") {
+      var index = 3
+    } else if (event.target.id === "l5") {
+      var index = 4
+    } else if (event.target.id === "l6") {
+      var index = 5
     }
 
-        ];
+    var end = index + 1
+    var oldMarkers = markers
+    markers = []
 
+    while (index < end) {
+      var position = locations[index].location
+      var title = locations[index].title
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 50.585499, lng: 5.988555},
-          zoom: 12,
-          styles: styles,
-          mapTypeControl: false
-        });
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
+        id: i
+      })
+      markers.push(marker)
+      //currentMarkers.push(markers);
+      index++
+      document
+        .getElementById(event.target.id)
+        .removeEventListener("click", showOne)
+      document.getElementById(event.target.id).style.display = "none"
+    }
+    //showListings();
+    var bounds = new google.maps.LatLngBounds()
 
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map)
+      bounds.extend(markers[i].position)
+    }
+    // Don't zoom in too far on only one marker from StackOverflow (https://stackoverflow.com/questions/3334729/google-maps-v3-fitbounds-zoom-too-close-for-single-marker)
+/*
+    if (bounds.getNorthEast().equals(bounds.getSouthWest())) {
+      var extendPoint1 = new google.maps.LatLng(
+        bounds.getNorthEast().lat() + 0.01,
+        bounds.getNorthEast().lng() + 0.01
+      )
+      var extendPoint2 = new google.maps.LatLng(
+        bounds.getNorthEast().lat() - 0.01,
+        bounds.getNorthEast().lng() - 0.01
+      )
+      bounds.extend(extendPoint1)
+      bounds.extend(extendPoint2)
+    }
+    */
+    map.fitBounds(bounds)
+    //
+    //currentMarkers.push(oldMarkers);
+    //currentMarkers.push(markers);
+    for (var i = 0; i < markers.length; i++) {
+      currentMarkers.push(markers[i])
+    }
+    //currentMarkers = markers;
+    markers = oldMarkers
 
-        var zoomAutocomplete = new google.maps.places.Autocomplete(
-            document.getElementById('zoom-to-area-text'));
+    l1Active = true
+  }
 
-        zoomAutocomplete.bindTo('bounds', map);
+  //TEST
+}
 
+function populateInfoWindow(marker, infowindow) {
+  if (infowindow.marker != marker) {
+    infowindow.setContent("")
+    infowindow.marker = marker
 
-        var locations = [
-          {title: 'Lac de la Gileppe', location: {lat: 50.589357, lng: 5.974609}},
-          {title: 'Parc naturel Hautes Fagnes - Eifel', location: {lat: 50.569816, lng: 6.091064}},
-          {title: 'Cimetière américain de Henri-Chapelle', location: {lat: 50.697335, lng: 5.900831}},
-          {title: 'Lac de Robertvile', location: {lat: 50.447490, lng: 6.122903}},
-          {title: 'Wesertalsperre', location: {lat: 50.6189468, lng: 6.0908503}},
-          {title: 'Spa aerodrome', location: {lat: 50.4792976, lng: 5.9067213}}
-        ];
+    infowindow.addListener("closeclick", function() {
+      infowindow.marker = null
+    })
 
-        var largeInfowindow = new google.maps.InfoWindow();
+    var streetViewService = new google.maps.StreetViewService()
+    var radius = 50
 
-        var defaultIcon = makeMarkerIcon('0091ff');
-
-
-        var highlightedIcon = makeMarkerIcon('FFFF24');
-
-        for (var i = 0; i < locations.length; i++) {
-
-          var position = locations[i].location;
-          var title = locations[i].title;
-
-          var marker = new google.maps.Marker({
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            icon: defaultIcon,
-            id: i
-          });
-
-          markers.push(marker);
-
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-
-          marker.addListener('mouseover', function() {
-            this.setIcon(highlightedIcon);
-          });
-          marker.addListener('mouseout', function() {
-            this.setIcon(defaultIcon);
-          });
-        }
-
-        document.getElementById('show-listings').addEventListener('click', showListings);
-
-        document.getElementById('hide-listings').addEventListener('click', function() {
-          hideMarkers(markers, currentMarkers);
-        });
-
-        document.getElementById('zoom-to-area').addEventListener('click', function() {
-          zoomToArea();
-        });
-
-        //TEST
-
-      //var one = 1;
-             document.getElementById('l1').addEventListener('click', showOne);
-             document.getElementById('l2').addEventListener('click', showOne);
-             document.getElementById('l3').addEventListener('click', showOne);
-             document.getElementById('l4').addEventListener('click', showOne);
-             document.getElementById('l5').addEventListener('click', showOne);
-             document.getElementById('l6').addEventListener('click', showOne);
-
-              function showOne() {
-
-
-                  if (event.target.id === 'l1'){
-                    var index=0;
-
-                  } else if (event.target.id === 'l2'){
-                    var index=1;
-
-                  } else if (event.target.id === 'l3'){
-                    var index=2;
-
-                  } else if (event.target.id === 'l4'){
-                    var index=3;
-
-                  } else if (event.target.id === 'l5'){
-                    var index=4;
-
-                  } else if (event.target.id === 'l6'){
-                    var index=5;
-
-                  }
-
-
-
-
-
-                var end = index+1;
-                var oldMarkers = markers;
-                markers = [];
-
-
-                  while(index < end){
-
-                  var position = locations[index].location;
-                  var title = locations[index].title;
-
-                  var marker = new google.maps.Marker({
-
-                    position: position,
-                    title: title,
-                    animation: google.maps.Animation.DROP,
-                    icon: defaultIcon,
-                    id: i
-                  });
-                  markers.push(marker);
-                  //currentMarkers.push(markers);
-                  index++
-                document.getElementById(event.target.id).removeEventListener("click", showOne);
-                document.getElementById(event.target.id).style.display = 'none'
-                }
-                //showListings();
-                var bounds = new google.maps.LatLngBounds();
-
-                for (var i = 0; i < markers.length; i++) {
-                  markers[i].setMap(map);
-                  bounds.extend(markers[i].position);
-
-                }
-                // Don't zoom in too far on only one marker from StackOverflow (https://stackoverflow.com/questions/3334729/google-maps-v3-fitbounds-zoom-too-close-for-single-marker)
-                if (bounds.getNorthEast().equals(bounds.getSouthWest())) {
-                  var extendPoint1 = new google.maps.LatLng(bounds.getNorthEast().lat() + 0.01, bounds.getNorthEast().lng() + 0.01);
-                  var extendPoint2 = new google.maps.LatLng(bounds.getNorthEast().lat() - 0.01, bounds.getNorthEast().lng() - 0.01);
-                  bounds.extend(extendPoint1);
-                  bounds.extend(extendPoint2);
-                }
-                map.fitBounds(bounds);
-              //
-              //currentMarkers.push(oldMarkers);
-              //currentMarkers.push(markers);
-              for (var i = 0; i < markers.length; i++) {
-                currentMarkers.push(markers[i]);
-              }
-              //currentMarkers = markers;
-              markers = oldMarkers;
-
-              l1Active = true;
-              }
-
-
-        //TEST
-
-      }
-
-
-      function populateInfoWindow(marker, infowindow) {
-
-        if (infowindow.marker != marker) {
-
-          infowindow.setContent('');
-          infowindow.marker = marker;
-
-          infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-          });
-
-
-          var streetViewService = new google.maps.StreetViewService();
-          var radius = 50;
-
-          function getStreetView(data, status) {
-            if (status == google.maps.StreetViewStatus.OK) {
-              var nearStreetViewLocation = data.location.latLng;
-              var heading = google.maps.geometry.spherical.computeHeading(
-                nearStreetViewLocation, marker.position);
-                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-                var panoramaOptions = {
-                  position: nearStreetViewLocation,
-                  pov: {
-                    heading: heading,
-                    pitch: 20
-                  }
-                };
-              var panorama = new google.maps.StreetViewPanorama(
-                document.getElementById('pano'), panoramaOptions);
-            } else {
-              infowindow.setContent('<div>' + marker.title + '</div>' +
-                '<div>No Street View Found</div>');
-            }
+    function getStreetView(data, status) {
+      if (status == google.maps.StreetViewStatus.OK) {
+        var nearStreetViewLocation = data.location.latLng
+        var heading = google.maps.geometry.spherical.computeHeading(
+          nearStreetViewLocation,
+          marker.position
+        )
+        infowindow.setContent(
+          "<div>" + marker.title + '</div><div id="pano"></div>'
+        )
+        var panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 20
           }
-
-          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-
-          infowindow.open(map, marker);
         }
+        var panorama = new google.maps.StreetViewPanorama(
+          document.getElementById("pano"),
+          panoramaOptions
+        )
+      } else {
+        infowindow.setContent(
+          "<div>" + marker.title + "</div>" + "<div>No Street View Found</div>"
+        )
       }
+    }
 
+    streetViewService.getPanoramaByLocation(
+      marker.position,
+      radius,
+      getStreetView
+    )
 
-      function showListings() {
-        var bounds = new google.maps.LatLngBounds();
+    infowindow.open(map, marker)
+  }
+}
 
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-          bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
-      }
+function showListings() {
+  var bounds = new google.maps.LatLngBounds()
 
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map)
+    bounds.extend(markers[i].position)
+  }
+  map.fitBounds(bounds)
+}
 
-      function hideMarkers(markers, currentMarkers) {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
+function hideMarkers(markers, currentMarkers) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null)
+  }
+  for (var i = 0; i < currentMarkers.length; i++) {
+    currentMarkers[i].setMap(null)
+  }
+}
 
-        }
-        for (var i = 0; i < currentMarkers.length; i++) {
-          currentMarkers[i].setMap(null);
-        }
-      }
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    "http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" +
+      markerColor +
+      "|40|_|%E2%80%A2",
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21, 34)
+  )
+  return markerImage
+}
 
+function zoomToArea() {
+  var geocoder = new google.maps.Geocoder()
 
-      function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
-          new google.maps.Size(21, 34),
-          new google.maps.Point(0, 0),
-          new google.maps.Point(10, 34),
-          new google.maps.Size(21,34));
-        return markerImage;
-      }
+  var address = document.getElementById("zoom-to-area-text").value
 
-
-      function zoomToArea() {
-
-        var geocoder = new google.maps.Geocoder();
-
-        var address = document.getElementById('zoom-to-area-text').value;
-
-        if (address == '') {
-          window.alert('You must enter an area, or address.');
+  if (address == "") {
+    window.alert("You must enter an area, or address.")
+  } else {
+    geocoder.geocode(
+      {
+        address: address,
+        componentRestrictions: { locality: "New York" }
+      },
+      function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location)
+          map.setZoom(15)
         } else {
-
-          geocoder.geocode(
-            { address: address,
-              componentRestrictions: {locality: 'New York'}
-            }, function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-                map.setZoom(15);
-              } else {
-                window.alert('We could not find that location - try entering a more' +
-                    ' specific place.');
-              }
-            });
-          }
+          window.alert(
+            "We could not find that location - try entering a more" +
+              " specific place."
+          )
         }
+      }
+    )
+  }
+}
